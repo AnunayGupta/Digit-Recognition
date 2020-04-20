d203294c78ca1bce8bd4e5e894e694a3321dab1c
AnunayGupta <42909609+AnunayGupta@users.noreply.github.com>
2020-04-20 19:09:42 +0530

    Add files via upload

---
+++ Neural Networks Digit Recognition/lrCostFunction.m
@@ -0,0 +1,75 @@
+function [J, grad] = lrCostFunction(theta, X, y, lambda)
+%LRCOSTFUNCTION Compute cost and gradient for logistic regression with 
+%regularization
+%   J = LRCOSTFUNCTION(theta, X, y, lambda) computes the cost of using
+%   theta as the parameter for regularized logistic regression and the
+%   gradient of the cost w.r.t. to the parameters. 
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+grad = zeros(size(theta));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost of a particular choice of theta.
+%               You should set J to the cost.
+%               Compute the partial derivatives and set grad to the partial
+%               derivatives of the cost w.r.t. each parameter in theta
+%
+% Hint: The computation of the cost function and gradients can be
+%       efficiently vectorized. For example, consider the computation
+%
+%           sigmoid(X * theta)
+%
+%       Each row of the resulting matrix will contain the value of the
+%       prediction for that example. You can make use of this to vectorize
+%       the cost function and gradient computations. 
+%
+% Hint: When computing the gradient of the regularized cost function, 
+%       there're many possible vectorized solutions, but one solution
+%       looks like:
+%           grad = (unregularized gradient for logistic regression)
+%           temp = theta; 
+%           temp(1) = 0;   % because we don't add anything for j = 0  
+%           grad = grad + YOUR_CODE_HERE (using the temp variable)
+%
+q = X * theta ; 
+q  = sigmoid(q) ; 
+p = log(q) ; 
+r = 1 - q ; 
+r = log(r) ; 
+r = (1-y) .* r ; 
+p = y .* p ; 
+s = p + r ;
+s = -s ; 
+s = s/m ; 
+B = theta .^2 ; 
+l = lambda / m ; 
+B = B * (l/2) ;  
+B(1) = 0 ;
+J = sum(s) + sum(B) ; 
+
+A= (X' * (q - y ))/m ;
+[g,h] = size(theta)  ; 
+
+C = zeros(g,1) ; 
+for u = 2 : g
+	C(u)  = l * theta(u) ; 
+end
+grad = A + C ; 
+
+
+
+
+
+
+
+
+
+% =============================================================
+
+grad = grad(:);
+
+end
